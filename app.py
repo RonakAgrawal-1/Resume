39ce99c2952e7da7eeeff7840adf7285cf7b4238
ronak <ronaka2328@gmail.com>
2023-09-14 01:48:32 +0530

    V4: added support for multiple file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 import re
 from PyPDF2 import PdfReader
 from io import BytesIO
@@ -37,51 +38,51 @@ def extract_username_from_url(github_link):
 # Function to fetch user repositories
 def fetch_user_repositories(github_link_or_gmail, resume_text):
     username = extract_username_from_url(github_link_or_gmail)
-    
+
     if username:
         github_user_url = f"https://api.github.com/users/{username}/repos"
         response = requests.get(github_user_url)
         if response.status_code == 200:
             return response.json()
-    
+
     email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
     gmail_match = re.search(email_pattern, resume_text)
-    
+
     if gmail_match:
         gmail_address = gmail_match.group()
         github_search_url = f"https://api.github.com/search/users?q={gmail_address}"
         response = requests.get(github_search_url)
-        
+
         if response.status_code == 200:
             search_results = response.json()
-            
+
             if "items" in search_results:
                 for item in search_results["items"]:
                     repos_url = item.get("repos_url")
-                    
+
                     if repos_url:
                         repos_response = requests.get(repos_url)
-                        
+
                         if repos_response.status_code == 200:
                             return repos_response.json()
-    
+
     if not username:
-        github_search_url = f"https://api.github.com/search/users?q={github_link_or_gmail.split('@')[0]}"
+        github_search_url = f"https://api.github.com/search/users?q={gmail_address.split('@')[0]}"
         response = requests.get(github_search_url)
-        
+
         if response.status_code == 200:
             search_results = response.json()
-            
+
             if "items" in search_results:
                 for item in search_results["items"]:
                     repos_url = item.get("repos_url")
-                    
+
                     if repos_url:
                         repos_response = requests.get(repos_url)
-                        
+
                         if repos_response.status_code == 200:
                             return repos_response.json()
-    
+
     return [{"name": "No repo found"}]
 
 # Function to extract text from a PDF file
@@ -126,35 +127,36 @@ def extract_job_description_skills(job_description_text):
 def calculate_matching_score(candidate_skills, job_description_text):
     job_skills = extract_job_description_skills(job_description_text)
     common_skills = list(set(candidate_skills) & set(job_skills))
-    
+
     if job_skills:
         score = len(common_skills) / len(job_skills)
     else:
         score = 0.0
-    
+
     return score, common_skills
 
-# Function to append new skill keyword to the skills_keywords.py file
+
+# Function to append new skill keyword to the skills_keywords.py file   
 def append_skill_keyword(new_skill):
     try:
         with open("skills_keywords.py", "r") as file:
             lines = file.readlines()
-        
+
         for i, line in enumerate(lines):
             if line.strip().startswith("skills_keywords = ["):
                 before_bracket = line[:line.index("[") + 1]
                 after_bracket = line[line.index("[") + 1:]
-                
+
                 if len(after_bracket.strip()) > 1:
                     lines[i] = before_bracket + after_bracket.rstrip() + f' "{new_skill}",\n'
                 else:
                     lines[i] = before_bracket + f'"{new_skill}",\n'
                 break
-        
+
         with open("skills_keywords.py", "w") as file:
             file.writelines(lines)
-        
-        reload(skills_keywords)
+
+        reload(skills_keywords)  # Reload the skills_keywords module to update the keyword list
         return True
     except Exception as e:
         return str(e)
@@ -172,86 +174,60 @@ st.title("Welcome to the Resume and GitHub Profile Analyzer!")
 st.sidebar.title("Resume and GitHub Analyzer")
 
 # Upload Resume Section in Sidebar
-st.sidebar.subheader("Step 1: Upload Resume")
-uploaded_file = st.sidebar.file_uploader("Upload your resume (PDF or DOC/DOCX)")
+st.sidebar.subheader("Step 1: Upload Resume Files")
+uploaded_files = st.sidebar.file_uploader("Upload your resume files (PDF or DOC/DOCX)", accept_multiple_files=True)
 
 # Enter Job Description Section in Sidebar
 st.sidebar.subheader("Step 2: Enter Job Description")
 job_description_text = st.sidebar.text_area("Enter the job description")
 
-# Create a Streamlit sidebar input for adding skills keywords
-st.sidebar.title("Manage Skills Keywords")
-new_skill = st.sidebar.text_input("Enter a new skill keyword")
+# ... (Your other setup code here)
 
-# Button to add the new skill keyword
-if st.sidebar.button("Add Skill Keyword"):
-    if append_skill_keyword(new_skill):
-        st.success(f"Skill keyword '{new_skill}' added successfully!")
-    else:
-        st.warning("Failed to add the skill keyword. Please try again.")
+# Display results for each candidate
+if uploaded_files:
+    # Create a DataFrame to store candidate information (name, matching score)
+    candidate_data = []
 
-# Analyze Button in Sidebar
-if st.sidebar.button("Analyze"):
-    if not uploaded_file or not job_description_text:
-        st.warning("Please complete both steps to analyze.")
-    else:
-        with st.spinner("Analyzing..."):
-            # Extract text from the uploaded resume file
-            if uploaded_file.type == "application/pdf":
-                resume_text = extract_text_from_pdf(uploaded_file)
-            elif uploaded_file.type in ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]:
-                resume_text = extract_text_from_docx(uploaded_file)
+    for uploaded_file in uploaded_files:
+        # Extract text from the uploaded resume file
+        if uploaded_file.type == "application/pdf":
+            resume_text = extract_text_from_pdf(uploaded_file)
+        elif uploaded_file.type in ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]:
+            resume_text = extract_text_from_docx(uploaded_file)
+        else:
+            st.warning(f"Skipping unsupported file: {uploaded_file.name}")
+            continue
+
+        # Calculate the matching score for the candidate
+        candidate_skills = extract_candidate_skills(resume_text)
+        score, common_skills = calculate_matching_score(candidate_skills, job_description_text)
+
+        # Append candidate information to the DataFrame
+        candidate_name = extract_candidate_name(resume_text)
+        candidate_data.append({"Name": candidate_name, "Matching Score": f"{int(score * 100)}%"})
+
+        # Create an expandable section for the candidate details
+        with st.expander(f"Details for {candidate_name} (Matching Score: {int(score * 100)}%)"):
+            # Display common skills, all skills, GitHub repos, GitHub link, Gmail link, etc.
+            st.subheader("Common Skills with Job Description")
+            if common_skills:
+                for skill in common_skills:
+                    st.write(f"- {skill}")
             else:
-                st.error("Unsupported file format. Please upload a PDF or DOC/DOCX file.")
-                st.stop()
-            
-            # Extract GitHub link from the candidate's resume text
+                st.write("No common skills found between the job description and the candidate's skills.")
+
+            st.subheader("Candidate Skills")
+            extracted_skills = extract_candidate_skills(resume_text)
+            if extracted_skills:
+                skills_text = ", ".join(extracted_skills)
+            else:
+                skills_text = "No skills found in the resume."
+            st.write(skills_text)
+
+            # Fetch and display GitHub repositories and other details if available
+            st.subheader("GitHub Repositories and Technologies")
             github_link = extract_github_link(resume_text)
-            
-            # Calculate the matching score based on skills
-            candidate_skills = extract_candidate_skills(resume_text)
-            score, common_skills = calculate_matching_score(candidate_skills, job_description_text)
-            
-            # Left Column (Name, GitHub Link, Common Skills, GitHub Repositories, Technologies)
-            st.subheader("Candidate Information")
-            col1, col2 = st.columns(2)
-            
-            # Candidate Name
-            with col1:
-                candidate_name = extract_candidate_name(resume_text)
-                st.write(f"Name: {candidate_name}")
-            
-            # GitHub Link
-            with col1:
-                if github_link:
-                    if not github_link.startswith("https://"):
-                        github_link = "https://" + github_link
-                    st.write(f"GitHub: [{github_link}]({github_link})")
-                else:
-                    st.write("No GitHub link found.")
-            
-            # Gmail Address
-            email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-            gmail_match = re.search(email_pattern, resume_text)
-            with col1:
-                if gmail_match:
-                    gmail_address = gmail_match.group()
-                    st.write(f"Gmail: {gmail_address}")
-                else:
-                    st.write("No Gmail address found.")
-            
-            # Common Skills
-            with col1:
-                st.subheader("Common Skills with Job Description")
-                if common_skills:
-                    for skill in common_skills:
-                        st.write(f"- {skill}")
-                else:
-                    st.write("No common skills found between the job description and the candidate's skills.")
-            
-            # GitHub Repositories and Technologies
-            with col2:
-                st.subheader("GitHub Repositories and Technologies")
+            if github_link:
                 github_repositories = fetch_user_repositories(github_link, resume_text)
                 if github_repositories:
                     repo_data = {
@@ -265,43 +241,38 @@ if st.sidebar.button("Analyze"):
                             repo_data["Repository Name"].append(repo_name)
                         if repo_language:
                             repo_data["Technologies Used"].append(repo_language)
-                    
+
                     max_length = max(len(repo_data["Repository Name"]), len(repo_data["Technologies Used"]))
                     repo_data["Repository Name"] += [""] * (max_length - len(repo_data["Repository Name"]))
                     repo_data["Technologies Used"] += [""] * (max_length - len(repo_data["Technologies Used"]))
-                    
+
                     repo_df = pd.DataFrame(repo_data)
                     st.write(repo_df)
                 else:
                     st.write(f"No repositories found for {github_link}.")
-            
-            # Right Column (Matching Score, All Skills)
-            st.subheader("Matching Score and All Skills")
-            col3, col4, col5 = st.columns(3)
-            
-            # Matching Score
-            with col3:
-                score_color = "green" if score >= 0.8 else "orange" if 0.5 <= score < 0.8 else "red"
-                st.markdown(
-                    f'<div style="text-align: center;">'
-                    f'<p style="font-size: 16px;">Matching Score</p>'
-                    f'<div style="border-radius: 50%; background-color: {score_color}; width: 80px; height: 80px; margin: 0 auto;">'
-                    f'<p style="font-size: 24px; color: white; line-height: 80px;">{int(score * 100)}%</p>'
-                    f'</div>'
-                    f'</div>',
-                    unsafe_allow_html=True
-                )
-            
-            # All Skills
-            with col4:
-                st.subheader("Candidate Skills")
-                extracted_skills = extract_candidate_skills(resume_text)
-                if extracted_skills:
-                    skills_text = ", ".join(extracted_skills)
-                else:
-                    skills_text = "No skills found in the resume."
-            
-                st.markdown(
-                    f'<p style="font-size: 14px;">{skills_text}</p>',
-                    unsafe_allow_html=True
-                )
+            else:
+                st.write("No GitHub link found.")
+
+            # Display Gmail address if available
+            email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+            gmail_match = re.search(email_pattern, resume_text)
+            if gmail_match:
+                gmail_address = gmail_match.group()
+                st.subheader("Gmail Address")
+                st.write(gmail_address)
+            else:
+                st.write("No Gmail address found.")
+else:
+    st.warning("Please upload resume files.")
+
+# Create a Streamlit sidebar input for adding skills keywords
+st.sidebar.title("Manage Skills Keywords")
+new_skill = st.sidebar.text_input("Enter a new skill keyword")
+
+# Button to add the new skill keyword
+if st.sidebar.button("Add Skill Keyword"):
+    if append_skill_keyword(new_skill):
+        st.success(f"Skill keyword '{new_skill}' added successfully!")
+    else:
+        st.warning("Failed to add the skill keyword. Please try again.")
+
